02c8017a7ab67dff35958c320ecd0a9a3ae6f712
Miyuki Watanabe <miyuki.wtnb@gmail.com>
2017-02-08 19:21:54 +0900

    Add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { HelloComponent } from './hello/hello.component';
+import { RouterModule }   from '@angular/router';
 
+import { HelloComponent } from './hello/hello.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -14,7 +15,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'hello',
+        component: HelloComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
